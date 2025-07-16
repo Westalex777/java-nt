3df8be53e62012dd24c0ebdab38f9a340fb6b1f8
root <westalex777@yandex.ru>
2025-07-16 16:17:44 +0300

    доработки appntwebflux

--- app-nt-webflux/src/main/java/com/example/appntwebflux/controller/NTController.java
+++ app-nt-webflux/src/main/java/com/example/appntwebflux/controller/NTController.java
@@ -4,7 +4,9 @@ import com.example.appntwebflux.service.NTService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -18,6 +20,12 @@ public class NTController {
     public Mono<String> test() {
         log.info("Request /test");
         return service.test();
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> stream( @RequestParam(name = "latency", required = false, defaultValue = "0") Integer latency) {
+        log.info("Request /stream");
+        return service.stream(latency);
     }
 
 }
--- app-nt-webflux/src/main/java/com/example/appntwebflux/service/NTService.java
+++ app-nt-webflux/src/main/java/com/example/appntwebflux/service/NTService.java
@@ -1,8 +1,10 @@
 package com.example.appntwebflux.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -12,29 +14,18 @@ public class NTService {
     private final WebClient webClient;
 
     public Mono<String> test() {
-        Mono<String> request1 = webClient.get()
+        return webClient.get()
                 .uri("/mock")
                 .retrieve()
                 .bodyToMono(String.class);
+    }
 
-        Mono<String> request2 = webClient.get()
-                .uri("/mock")
+    public Flux<String> stream(Integer latency) {
+        return webClient.get()
+                .uri("/stream?latency=" + latency)
+                .accept(MediaType.TEXT_EVENT_STREAM)
                 .retrieve()
-                .bodyToMono(String.class);
-
-        Mono<String> request3 = webClient.get()
-                .uri("/mock")
-                .retrieve()
-                .bodyToMono(String.class);
-
-        // Выполнит все три запроса параллельно и соберет результат, когда все завершатся
-        return Mono.zip(request1, request2, request3)
-                .map(tuple3 -> {
-                    String r1 = tuple3.getT1();
-                    String r2 = tuple3.getT2();
-                    String r3 = tuple3.getT3();
-                    return "Results:\n" + r1 + "\n" + r2 + "\n" + r3;
-                });
+                .bodyToFlux(String.class);
     }
 
 }
