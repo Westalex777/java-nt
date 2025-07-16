dced9fa61997e7cfabe2a5b116d5c1449ce7ba32
root <westalex777@yandex.ru>
2025-07-16 20:51:56 +0300

    webflux config

--- app-nt-webflux/src/main/java/com/example/appntwebflux/AppNtWebfluxApplication.java
+++ app-nt-webflux/src/main/java/com/example/appntwebflux/AppNtWebfluxApplication.java
@@ -2,6 +2,9 @@ package com.example.appntwebflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.resources.LoopResources;
 
 @SpringBootApplication
 public class AppNtWebfluxApplication {
@@ -10,4 +13,12 @@ public class AppNtWebfluxApplication {
         SpringApplication.run(AppNtWebfluxApplication.class, args);
     }
 
+    @Bean
+    public NettyReactiveWebServerFactory nettyFactory() {
+        NettyReactiveWebServerFactory factory = new NettyReactiveWebServerFactory();
+        factory.addServerCustomizers(httpServer ->
+                httpServer.runOn(LoopResources.create("my-netty", 200, true))
+        );
+        return factory;
+    }
 }
--- app-nt-webflux/src/main/java/com/example/appntwebflux/client/WebClientConfiguration.java
+++ app-nt-webflux/src/main/java/com/example/appntwebflux/client/WebClientConfiguration.java
@@ -3,7 +3,12 @@ package com.example.appntwebflux.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfiguration {
@@ -13,6 +18,14 @@ public class WebClientConfiguration {
 
     @Bean
     public WebClient webClient() {
-        return WebClient.create(url);
+        ConnectionProvider provider = ConnectionProvider.builder("custom")
+                .maxConnections(2000)
+                .pendingAcquireTimeout(Duration.ofSeconds(30))
+                .build();
+
+        return WebClient.builder()
+                .baseUrl(url)
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create(provider)))
+                .build();
     }
 }
