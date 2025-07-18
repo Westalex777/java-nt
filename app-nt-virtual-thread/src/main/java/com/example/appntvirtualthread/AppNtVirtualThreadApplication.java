1389123715a7492ec9b8da41773e804f3d43fd07
root <westalex777@yandex.ru>
2025-07-18 21:33:05 +0300

    доработки

--- app-nt-virtual-thread/src/main/java/com/example/appntvirtualthread/AppNtVirtualThreadApplication.java
+++ app-nt-virtual-thread/src/main/java/com/example/appntvirtualthread/AppNtVirtualThreadApplication.java
@@ -2,30 +2,12 @@ package com.example.appntvirtualthread;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.support.TaskExecutorAdapter;
-
-import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class AppNtVirtualThreadApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppNtVirtualThreadApplication.class, args);
-    }
-
-    @Bean
-    public AsyncTaskExecutor applicationTaskExecutor() {
-        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
-    }
-
-    @Bean
-    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
-        return protocolHandler -> {
-            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
-        };
     }
 
 }
--- app-nt-virtual-thread/src/main/java/com/example/appntvirtualthread/client/http/HttpWebClient.java
+++ app-nt-virtual-thread/src/main/java/com/example/appntvirtualthread/client/http/HttpWebClient.java
@@ -1,8 +1,12 @@
 package com.example.appntvirtualthread.client.http;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
 
 @Component
 public class HttpWebClient implements HttpClient {
@@ -10,14 +14,33 @@ public class HttpWebClient implements HttpClient {
     @Value("${integration.mock.url}")
     private String url;
 
-    private final WebClient webClient = WebClient.create();
+    private WebClient webClient;
 
     @Override
     public String mockIntegration() {
+        install();
         return webClient.get()
                 .uri(url + "/mock")
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
+    }
+
+    private void install() {
+        if (this.webClient == null) {
+            this.webClient = create();
+        }
+    }
+
+    private WebClient create() {
+        ConnectionProvider provider = ConnectionProvider.builder("custom")
+                .maxConnections(2500)
+                .pendingAcquireTimeout(Duration.ofSeconds(30))
+                .build();
+
+        return WebClient.builder()
+                .baseUrl(url)
+                .clientConnector(new ReactorClientHttpConnector(reactor.netty.http.client.HttpClient.create(provider)))
+                .build();
     }
 }
